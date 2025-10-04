0fa6b054e98e874ce0634f267b45ee75fca931da
alexheffesse <alex.heffesse@gmail.com>
2025-10-04 13:09:44 -0400

    Mini.tsx

---
+++ app/mini/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:24}}>
+      <h1>Mini App Entry</h1>
+      <p>This route will host the in-feed experience.</p>
+    </main>
+  );
+}
