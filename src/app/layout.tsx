907d9310bd6def260071c0734f2bbd0b36cdb3e6
alexheffesse <alex.heffesse@gmail.com>
2025-10-04 15:05:49 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,8 @@ import { NEXT_PUBLIC_URL } from '../config';
 import './global.css';
 import '@coinbase/onchainkit/styles.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import dynamic from 'next/dynamic';
 
-const OnchainProviders = dynamic(
-  () => import('src/components/OnchainProviders'),
-  {
-    ssr: false,
-  },
-);
+import Providers from 'src/components/Providers';
 
 export const viewport = {
   width: 'device-width',
@@ -34,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex items-center justify-center">
-        <OnchainProviders>{children}</OnchainProviders>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
