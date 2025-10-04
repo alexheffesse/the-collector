2e80466daf1a23733b6df88fdd7f5fc3771015f7
alexheffesse <alex.heffesse@gmail.com>
2025-10-04 13:16:35 -0400

    Create page.tsx

---
+++ app/rewards/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:24}}>
+      <h1>Rewards</h1>
+      <p>Non-financial XP & badges (Founding Voter, Curator’s Pick, etc.).</p>
+    </main>
+  );
+}
