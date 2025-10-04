6c1ff8d34b4813ba4646bdf07abea2b227c7b1d4
alexheffesse <alex.heffesse@gmail.com>
2025-10-04 13:16:09 -0400

    Create page.tsx

---
+++ app/vote/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <main style={{padding:24}}>
+      <h1>Vote</h1>
+      <ol>
+        <li>Discovery poll (shortlist)</li>
+        <li>Final vote (on-chain)</li>
+      </ol>
+    </main>
+  );
+}
